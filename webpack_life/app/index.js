8df029a04f592c1314728b4e701312d1cf01d876
J. Milam Walters <jmilamwalters@gmail.com>
2018-04-06 16:29:10 -0500

    Refactor to evaluate only living cells

--- webpack_life/app/index.js
+++ webpack_life/app/index.js
@@ -31,6 +31,10 @@ function neighborhood([x = 0, y = 0] = []) {
   )
 }
 
+function neighbors(cell = ([x = 0, y = 0] = [])) {
+  return without([cell], neighborhood(cell))
+}
+
 function consGrid(size = 1) {
   return reduce(
     (acc, x) => concat(acc, map(y => [x, y], range(0, size))),
@@ -56,7 +60,43 @@ function paint(arrIn = [], w = 10) {
   )
 }
 
-function step(world = [], arrIn = []) {
+function resolveDuplicates(arr = []) {
+  let hash = {}
+  let result = []
+
+  for (let i = 0; i < arr.length; ++i) {
+    if (!hash.hasOwnProperty(arr[i])) {
+      hash[arr[i]] = true
+      result.push(arr[i])
+    }
+  }
+
+  return result
+}
+
+// function step(world = [], arrIn = []) {
+//   const arrOut = reduce(
+//     (acc, curr) =>
+//       cond([
+//         [equals(3), always([curr, ...acc])],
+//         [equals(4), () => (contains(curr, arrIn) ? [curr, ...acc] : acc)],
+//         [T, always(acc)],
+//       ])(intersection(neighborhood(curr), arrIn).length),
+//     [],
+//     world
+//   )
+//   paint(arrOut)
+//   // return requestAnimationFrame(() => step(world, arrOut))
+//   return requestAnimationFrame(() =>
+//     measureTime('step', () => step(world, arrOut))
+//   )
+// }
+
+// function step(world = [], arrIn = []) {
+function step(arrIn = []) {
+  const cellsToEval = resolveDuplicates(
+    reduce((acc, curr) => [...acc, ...neighborhood(curr)], [], arrIn)
+  )
   const arrOut = reduce(
     (acc, curr) =>
       cond([
@@ -65,13 +105,16 @@ function step(world = [], arrIn = []) {
         [T, always(acc)],
       ])(intersection(neighborhood(curr), arrIn).length),
     [],
-    world
+    // world
+    cellsToEval
   )
   paint(arrOut)
   // return requestAnimationFrame(() => step(world, arrOut))
   return requestAnimationFrame(() =>
-    measureTime('step', () => step(world, arrOut))
+    // measureTime('step', () => step(world, arrOut))
+    measureTime('step', () => step(arrOut))
   )
 }
 
-;(() => step(consGrid(25), seedRandom(consGrid(25))))()
+// ;(() => step(consGrid(25), seedRandom(consGrid(25))))()
+;(() => step(seedRandom(consGrid(25))))()
